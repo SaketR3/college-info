b64bf2ebcb7b547133fe3fe5638501886e09dcfc
SaketR3 <saketr3@illinois.edu>
2024-02-24 20:37:54 -0600

    Add generative AI functionality

--- college-info/src/Advice.jsx
+++ college-info/src/Advice.jsx
@@ -1,5 +1,42 @@
+import OpenAI from 'openai';
+import "dotenv"
+import { useEffect, useState } from 'react';
+
+const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_API_KEY,
+    dangerouslyAllowBrowser: true
+});
+
 export default function Advice(props) {
-    return (
-      <p>{props.prompt}</p>
-    );
+    const [advice, setAdvice] = useState([]);
+    const [showAdvice, setShowAdvice] = useState(false);
+    
+    const prompt = props.prompt + "Please seperate each point with the word 'Step'. Include an introduction and be conversational, but don't include any ending remarks.";
+
+    useEffect(() => {
+        (async () => {
+            const chatCompletion = await openai.chat.completions.create({
+            messages: [{ role: 'user', content: prompt }],
+            model: 'gpt-3.5-turbo',
+            });
+            const apiResponse = chatCompletion.choices[0].message.content;
+
+            setAdvice(apiResponse.split("Step"));
+            setShowAdvice(true);
+        })();
+    }, []);
+    
+    return (<>
+        {showAdvice ? 
+            <ul>
+              {advice.map((step, index) => 
+                index == 0 ? <p className="intro" key={index}>{step}</p> :
+                <li key={index}>{step}</li>
+              )}
+            </ul>
+            : 
+            <div>
+              <p>Loading...</p>
+            </div>}
+    </>);
 }
--- college-info/src/App.jsx
+++ college-info/src/App.jsx
@@ -8,20 +8,28 @@ export default function App() {
   const [prompt, setPrompt] = useState('');
   
   function generateAdvice(prompt) {
-    setShowAdvice(true);
+    setShowAdvice(false);
+    setTimeout(() => {setShowAdvice(true)}, 0);
     setPrompt(prompt);
   }
 
   return (
     <div>
-      <Button text="How can I get involved on campus?" prompt="How can I get involved on campus in college?" generateAdvice={generateAdvice} />
-      <Button text="How can I improvement my time management?"  prompt="How can I improvement my time management in college?" generateAdvice={generateAdvice} />
-      <Button text="What steps should I take to get an internship?" prompt="What steps should I take to get an internship in college?" generateAdvice={generateAdvice} />
-      <Button text="How do I prepare for a career fair?" prompt="How do I prepare for a career fair in college?" generateAdvice={generateAdvice} />
-      <Button text="What steps should I take to get started with research?" prompt="What steps should I take to get started with research in college?" generateAdvice={generateAdvice} />
-      <Button text="How can I get involved with hackathons?" prompt="How can I get involved with hackathons in college?" generateAdvice={generateAdvice} />
-      <Button text="What types of extracurriculars are there in college?" prompt="What types of extracurriculars are there in college?" generateAdvice={generateAdvice} />
-      <Button text="What types of events are there in college?" prompt="What types of events are there in college?" generateAdvice={generateAdvice} />
+      <h1>College Personal Assistant</h1>
+      <p className="info">Use the power of generative AI to help you navigate through your college journey!</p>
+      <p className="info">Simply click a button and receive advice!</p>
+
+      <div className="buttons">
+        <Button text="How can I get involved on campus?" prompt="How can I get involved on campus in college?" generateAdvice={generateAdvice} />
+        <Button text="How can I improvement my time management?"  prompt="How can I improvement my time management in college?" generateAdvice={generateAdvice} />
+        <Button text="What steps should I take to get an internship?" prompt="What steps should I take to get an internship in college?" generateAdvice={generateAdvice} />
+        <Button text="How do I prepare for a career fair?" prompt="How do I prepare for a career fair in college?" generateAdvice={generateAdvice} />
+        <Button text="What steps should I take to get started with research?" prompt="What steps should I take to get started with research in college?" generateAdvice={generateAdvice} />
+        <Button text="How can I get involved with hackathons?" prompt="How can I get involved with hackathons in college?" generateAdvice={generateAdvice} />
+        <Button text="What types of extracurriculars are there in college?" prompt="What types of extracurriculars are there in college?" generateAdvice={generateAdvice} />
+        <Button text="What types of events are there in college?" prompt="What types of events are there in college?" generateAdvice={generateAdvice} />
+      </div>
+      
       {showAdvice && <Advice prompt={prompt} />}
     </div>
   );
